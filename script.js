3c53e44026f1b7a5589754438b855d9bd4681324
thabadcode <tchekgames@gmail.com>
2026-04-09 13:53:58 -0300

    feat: Adiciona animação ao ícone do menu hambúrguer transformando em seta.

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const menuHamburg = document.getElementById('menu-hamburg');
 
 iconMenuHamburg.addEventListener('click', () => {
     menuHamburg.classList.toggle('active');
+    iconMenuHamburg.classList.toggle('active');
 });
