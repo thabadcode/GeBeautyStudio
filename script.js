f231e23affd5ff03a910f2dbb5c609f4f3ccac4c
thabadcode <tchekgames@gmail.com>
2026-04-22 08:51:39 -0300

    feat: Adiciona adição da classe do body e adiciona a ação de fechar o menu aos outros elementos.

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const iconMenuHamburg = document.getElementById('icon-menu-hamburg');
 const menuHamburg = document.getElementById('menu-hamburg');
 const backdrop = document.getElementById('backdrop');
+let isMenuShow = false;
 
-iconMenuHamburg.addEventListener('click', () => { showMenu(); });
+iconMenuHamburg.addEventListener('click', () => {
+    if (isMenuShow) closeMenu();
+    else showMenu();
+});
+
+menuHamburg.addEventListener('click', (event) => {
+    const elementClicked = event.target.closest('a');
+    if (elementClicked) closeMenu();
+})
+
+backdrop.addEventListener('click', () => { closeMenu() });
 
 function showMenu() {
-    menuHamburg.classList.toggle('active');
-    iconMenuHamburg.classList.toggle('active');
-    backdrop.classList.toggle('active');
+    menuHamburg.classList.add('active');
+    iconMenuHamburg.classList.add('active');
+    backdrop.classList.add('active');
+    document.body.classList.add('stop-scroll');
+    isMenuShow = true;
+}
+
+function closeMenu() {
+     menuHamburg.classList.remove('active');
+    iconMenuHamburg.classList.remove('active');
+    backdrop.classList.remove('active');
+    document.body.classList.remove('stop-scroll');
+    isMenuShow = false;
 }
