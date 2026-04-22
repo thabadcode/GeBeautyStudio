df8ff0b2979f8cca8a6a51b6c58dd564a4fa407e
thabadcode <tchekgames@gmail.com>
2026-04-22 08:01:06 -0300

    feat: Adiciona backdrop para escurecer o fundo quando o menu estiver aberto.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const iconMenuHamburg = document.getElementById('icon-menu-hamburg');
 const menuHamburg = document.getElementById('menu-hamburg');
+const backdrop = document.getElementById('backdrop');
 
-iconMenuHamburg.addEventListener('click', () => {
+iconMenuHamburg.addEventListener('click', () => { showMenu(); });
+
+function showMenu() {
     menuHamburg.classList.toggle('active');
     iconMenuHamburg.classList.toggle('active');
-});
+    backdrop.classList.toggle('active');
+}
